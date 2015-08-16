2840a93ee3ae14b8d9ba95354bcd39f3838f4a1a
Harrison Qu <harrisonqu96@gmail.com>
2015-08-15 18:56:11 -0700

    demo

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/wat', function(req, res, next) {
+  res.render('spare', { title: 'Spare Change' });
 });
 
 /* GET Hello World page. */
@@ -12,7 +12,7 @@ router.get('/helloworld', function(req, res) {
 });
 
 /*GET home page*/
-router.get('/spare', function(req, res) {
+router.get('/', function(req, res) {
     res.render('spare', { title: 'Spare Change' });
 });
 
